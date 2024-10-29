ed263a98473937c8fb4f8cc9e2f8e9869907ea34
Peter Kaltzis <peterkaltzis@outlook.com>
2024-10-29 17:51:58 +1100

    removed Menu component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import React from "react";
 
 import CheckTracks from "./Pages/CheckTracks";
-import Menu from "./Components/Menu";
 
 function App(): React.JSX.Element {
     return (
@@ -13,7 +12,6 @@ function App(): React.JSX.Element {
                     <Route element={<CheckTracks />} path="check-tracks" />
                 </Routes>
             </main>
-            <Menu />
         </BrowserRouter>
     );
 }
