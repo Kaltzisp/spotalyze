ae124be043efbf640c0593e09fa2ade0b30aab34
Peter Kaltzis <peterkaltzis@outlook.com>
2024-10-29 18:29:51 +1100

    added playlistToCSV

--- src/Pages/CheckTracks.tsx
+++ src/Pages/CheckTracks.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { getPlaylist } from "src/services/spotifyApi";
+import { getReleaseDates, playlistToCSV } from "src/services/spotifyApi";
 
 function CheckTracks(): React.JSX.Element {
     const [playlistUrl, setPlaylistUrl] = useState("");
+
     return (
         <div>
             <input name="playlistUrl" onChange={(e): void => setPlaylistUrl(e.target.value)} placeholder="Spotify Playlist URL" type="text" value={playlistUrl} />
             <button onClick={(): void => {
-                getPlaylist(playlistUrl).catch(e => console.error(e));
+                getReleaseDates(playlistUrl).catch(e => console.error(e));
             }} type="button">{"Check release dates"}
+            </button>
+            <button onClick={(): void => {
+                playlistToCSV(playlistUrl).catch(e => console.error(e));
+            }} type="button">{"Playlist to CSV"}
             </button>
         </div>
     );
--- src/services/spotifyApi.ts
+++ src/services/spotifyApi.ts
@@ -1,4 +1,4 @@
-import { type Track, getTrackInfo } from "./geniusApi";
+import { getTrackInfo } from "./geniusApi";
 
 interface SpotifyTrack {
     track: {
@@ -10,11 +10,9 @@ interface SpotifyTrack {
 }
 
 interface PlaylistResponse {
-    tracks: {
-        items: SpotifyTrack[];
-        offset: number;
-        total: number;
-    };
+    items: SpotifyTrack[];
+    offset: number;
+    total: number;
 }
 
 export async function getSpotifyToken(): Promise<string> {
@@ -32,34 +30,41 @@ export async function getSpotifyToken(): Promise<string> {
     return data.access_token;
 }
 
-async function getFullPlaylist(url: string): Promise<Track[]> {
+async function getFullPlaylist(url: string): Promise<SpotifyTrack[]> {
     const token = await getSpotifyToken();
-    const playlistId = url.split("/").pop();
+    const playlistId = url.split("/").pop()?.split("?").shift();
     let allTracksConsumed = false;
     let currentOffset = 0;
-    const trackPromises: Promise<Track>[] = [];
+    const tracks: SpotifyTrack[] = [];
     while (!allTracksConsumed) {
-        const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}?offset=${currentOffset}`, {
+        const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=${currentOffset}`, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         });
         const playlistInfo = await response.json() as PlaylistResponse;
-        playlistInfo.tracks.items.forEach(item => trackPromises.push(getTrackInfo(item.track.name, item.track.artists[0].name)));
-        if (playlistInfo.tracks.offset + 100 >= playlistInfo.tracks.total) {
+        playlistInfo.items.forEach(item => tracks.push(item));
+        if (playlistInfo.offset + 100 >= playlistInfo.total) {
             allTracksConsumed = true;
         } else {
             currentOffset += 100;
         }
     }
-    const tracks = await Promise.all(trackPromises);
     return tracks;
 }
 
-export async function getPlaylist(url: string): Promise<void> {
-    const tracks = await getFullPlaylist(url);
+export async function getReleaseDates(url: string): Promise<void> {
+    const spotifyTracks = await getFullPlaylist(url);
+    const trackPromises = spotifyTracks.map(async track => getTrackInfo(track.track.name, track.track.artists[0].name));
+    const tracks = await Promise.all(trackPromises);
     tracks.sort((a, b) => new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime());
     tracks.forEach((track) => {
         console.log(`${track.query} : ${track.name} : ${track.artists} : ${track.releaseDate}`);
     });
+}
+
+export async function playlistToCSV(url: string): Promise<void> {
+    const tracks = await getFullPlaylist(url);
+    const csvData = tracks.map(track => `${track.track.name}\t${track.track.artists.map(artist => artist.name).join("; ")}`).join("\n");
+    console.log(csvData);
 }
