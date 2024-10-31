b1b28b292b10ddef7b58e90b8eb6e28a85cd8eaf
Peter Kaltzis <peterkaltzis@outlook.com>
2024-10-31 15:41:24 +1100

    improved index.html

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Playlist } from "./Classes/Playlist.js";
-import { config } from "dotenv";
-
-config();
-
-const playlist = new Playlist("https://open.spotify.com/playlist/52DDYLZ2am85rMRlYfPqdY?si=1a11b591d14a471f");
-await playlist.getTracks();
