fa6cb502262a05ecb6d0d07f0d796cf6120dc757
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-12 16:19:06 +1100

    added results submission

---
+++ src/app/api/results/submit/route.ts
@@ -0,0 +1,64 @@
+import type { NextRequest } from "next/server";
+import type { TextFile } from "@/app/spotalyze/page";
+import { parse } from "papaparse";
+import { writeFileSync } from "fs";
+
+interface Results {
+    [trackId: string]: {
+        [owner: string]: {
+            rank: number;
+            notes: string;
+        };
+    };
+}
+
+export async function POST(request: NextRequest): Promise<Response> {
+    const files = await request.json() as TextFile[];
+
+    // Parsing submissions from CSV files.
+    const submissions = files.map((file) => {
+        const csvData = parse(file.content).data as unknown as string[][];
+        const headers = csvData[0].map((header) => header.trim().toLowerCase());
+        const idColumn = headers.indexOf("spotify id");
+        const rankColumn = headers.indexOf("rank");
+        const notesColumn = headers.indexOf("notes");
+        if (idColumn === -1 || rankColumn === -1 || notesColumn === -1) {
+            console.error(`Failed to parse headers from ${file.name}`);
+            return null;
+        }
+        const ranking: (string | undefined)[] = new Array<string>(160);
+        const notes: (string | undefined)[] = new Array<string>(160);
+        for (let i = 1; i <= 160; i++) {
+            const trackRank = parseInt(csvData[i][rankColumn], 10);
+            ranking[trackRank - 1] = csvData[i][idColumn];
+            notes[trackRank - 1] = csvData[i][notesColumn];
+        }
+        if (ranking.some((trackId) => typeof trackId === "undefined")) {
+            console.error(`Failed to parse rankings from ${file.name}`);
+            return null;
+        }
+        return {
+            owner: file.name,
+            ranking: ranking as string[],
+            notes: notes as string[]
+        };
+    }).filter((submission) => submission !== null);
+
+    // Collating submissions into scores.
+    const results: Results = {};
+    submissions.forEach((submission) => {
+        submission.ranking.forEach((trackId, index) => {
+            if (typeof results[trackId] === "undefined") {
+                results[trackId] = {};
+            }
+            results[trackId][submission.owner] = {
+                rank: index + 1,
+                notes: submission.notes[index]
+            };
+        });
+    });
+
+    writeFileSync("./data/Results.json", JSON.stringify(results, null, 4));
+
+    return new Response("OK", { status: 200 });
+}
