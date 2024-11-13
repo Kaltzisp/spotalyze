17a00559d9e690a5342a3bf3abbdeabe72619bee
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-14 04:55:50 +1100

    refactored TrackInfo component

--- src/app/soty/slideshow/Components/TrackInfo.tsx
+++ src/app/soty/slideshow/Components/TrackInfo.tsx
@@ -3,16 +3,16 @@ import { JSDate } from "@/app/api/playlists/JSDate";
 import type { RankedTrack } from "@/app/api/playlists/submit-results/route";
 
 interface TrackInfoProps {
+    readonly fadeDuration: number;
     readonly quoteDuration: number;
-    readonly nQuotes: number;
     readonly track: RankedTrack;
     readonly trackIndex: number;
     readonly tracks: RankedTrack[];
+    readonly visible: boolean;
 }
 
 export default function TrackInfo(props: TrackInfoProps): React.JSX.Element {
     const [scoreVisible, setScoreVisible] = React.useState(false);
-    const track = props.track;
 
     function getUser(id: string): string {
         switch (id) {
@@ -30,39 +30,41 @@ export default function TrackInfo(props: TrackInfoProps): React.JSX.Element {
 
     useEffect(() => {
         if (typeof props.track !== "undefined") {
+            const nQuotes = Object.values(props.track.scores).map((result) => result.notes).filter((note) => note !== "").length;
+            setScoreVisible(false);
             setTimeout(() => {
                 setScoreVisible(true);
-            }, props.quoteDuration * (props.nQuotes + Object.keys(track.scores).length));
+            }, props.quoteDuration * (nQuotes + Object.keys(props.track.scores).length + 1));
         }
     }, [props.track]);
 
     return (
-        <div className="flex gap-10 justify-center">
-            <img className="max-w-[512px] w-[512px] flex-1 border-white border box-border" src={track.albumImageUrl} />
+        <div className={`flex gap-10 justify-center duration-${props.fadeDuration} ease-in-out ${props.visible ? "opacity-100" : "invisible opacity-0"}`}>
+            <img className="max-w-[512px] w-[512px] flex-1 border-white border box-border" src={props.track.albumImageUrl} />
             <div className="max-w-[512px] w-[512px] flex-1 flex flex-col gap-5">
                 {typeof props.trackIndex === "number" ? <span className="text-4xl">
-                    {`# ${isTied(track) ? "=" : ""}${track.place}`}
+                    {`# ${isTied(props.track) ? "=" : ""}${props.track.place}`}
                 </span> : null}
-                <span className="text-5xl">{track.name}</span>
-                <span className="text-xl">{track.artists.replaceAll(";", ",")}</span>
+                <span className="text-5xl">{props.track.name}</span>
+                <span className="text-xl">{props.track.artists.replaceAll(";", ",")}</span>
                 <div className="flex flex-col justify-end h-full text-xl">
                     <span className="flex">
                         <p className="w-[150px]">{"Date released: "}</p>
-                        <p className="w-[150px] text-center">{new JSDate(track.dateReleased).dateString()}</p>
+                        <p className="w-[150px] text-center">{new JSDate(props.track.dateReleased).dateString()}</p>
                     </span>
                     <span className="flex">
                         <p className="w-[150px]">{"Date added: "}</p>
-                        <p className="w-[150px] text-center">{new JSDate(track.dateAdded).dateString()}</p>
+                        <p className="w-[150px] text-center">{new JSDate(props.track.dateAdded).dateString()}</p>
                     </span>
                     <span className="flex mt-5">
                         <p className="w-[150px]">{"Days to add: "}</p>
-                        <p className="w-[150px] text-center">{Math.max(Math.floor((new Date(track.dateAdded).getTime() - new Date(track.dateReleased).getTime()) / (1000 * 60 * 60 * 24)), 0)}</p>
+                        <p className="w-[150px] text-center">{Math.max(Math.floor((new Date(props.track.dateAdded).getTime() - new Date(props.track.dateReleased).getTime()) / (1000 * 60 * 60 * 24)), 0)}</p>
                     </span>
                     <span className="flex">
                         <p className="w-[150px]">{"Added by: "}</p>
-                        <p className="w-[150px] text-center">{getUser(track.addedBy)}</p>
+                        <p className="w-[150px] text-center">{getUser(props.track.addedBy)}</p>
                     </span>
-                    <span className={`flex mt-10 mb-2 text-3xl duration-1000 ease-in-out ${scoreVisible ? "opacity-100" : "invisible opacity-0"}`}>
+                    <span className={`flex mt-10 mb-2 text-3xl duration-${props.fadeDuration} ease-in-out ${scoreVisible ? "opacity-100" : "invisible opacity-0"}`}>
                         <p className="w-[150px]">{"Score: "}</p>
                         <p className="w-[150px] text-center">{props.track.total}</p>
                     </span>
