2e3d942e97c7414b40f39bfea902e8af5492b05e
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-14 03:10:47 +1100

    added Quote component

---
+++ src/app/soty/slideshow/Components/Quote.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+interface QuoteProps {
+    readonly quotes: string[];
+    readonly quoteDuration: number;
+    readonly fadeDuration: number;
+}
+
+export default function Quote(props: QuoteProps): React.JSX.Element {
+    const [quote, setQuote] = useState<string>();
+    const [quoteVisible, setQuoteVisible] = useState(false);
+
+    function randomPick<T>(arr: T[]): T {
+        return arr.splice(Math.floor(Math.random() * arr.length), 1)[0];
+    }
+
+    function cycleQuote(string: string): void {
+        setQuote(string);
+        setQuoteVisible(true);
+        setTimeout(() => setQuoteVisible(false), props.quoteDuration - props.fadeDuration);
+    }
+
+    useEffect(() => {
+        if (props.quotes.length > 0) {
+            const quoteList = [...props.quotes];
+            cycleQuote(randomPick(quoteList));
+            const interval = setInterval(() => {
+                if (quoteList.length > 0) {
+                    cycleQuote(randomPick(quoteList));
+                } else {
+                    clearInterval(interval);
+                }
+            }, props.quoteDuration);
+        }
+    }, [props.quotes]);
+
+    return (
+        <span className={`fixed pl-20 pr-20 text-5xl font-serif duration-${props.fadeDuration} ease-in-out text-justify ${quoteVisible ? "opacity-100" : "invisible opacity-0"}`}>
+            {typeof quote === "undefined" ? null : `“ ${quote.trim()} ”`}
+        </span>
+    );
+}
