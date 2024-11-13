442e5057b43a70c9dadd244a09002aa276881781
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-13 20:10:19 +1100

    added image upload scope

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -8,7 +8,7 @@ export function GET(): Response {
         client_id: process.env.SPOTIFY_CLIENT_ID,
         redirect_uri: spotifyRedirectUri,
         response_type: "code",
-        scope: "playlist-modify-public"
+        scope: "playlist-modify-public ugc-image-upload"
     });
     return Response.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 }
