9eff1e62362ea3ac85cabade5d329f6b5049fd73
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-13 13:56:42 +1100

    added JSDate.dateString method

--- src/app/api/playlists/JSDate.ts
+++ src/app/api/playlists/JSDate.ts
@@ -2,4 +2,11 @@ export class JSDate extends Date {
     public toExcelDate(): number {
         return (this.getTime() - new Date("1899-12-30").getTime()) / (1000 * 60 * 60 * 24);
     }
+
+    public dateString(): string {
+        const date = this.getDate() < 10 ? `0${this.getDate()}` : this.getDate();
+        const month = this.getMonth() + 1 < 10 ? `0${this.getMonth() + 1}` : this.getMonth() + 1;
+        const year = this.getFullYear();
+        return `${date}-${month}-${year}`;
+    }
 }
