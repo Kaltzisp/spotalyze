c5f8b89e8c28bfc023c249875e9602eb69dbdf8d
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-14 11:49:22 +1100

    fixed UserRanks fade in

--- src/app/soty/slideshow/Components/UserRanks.tsx
+++ src/app/soty/slideshow/Components/UserRanks.tsx
@@ -2,16 +2,23 @@ import React, { useEffect } from "react";
 import type { RankedTrack } from "@/app/api/playlists/submit-results/route";
 import { shuffle } from "@/app/api/utils";
 
-export default function UserRanks(props: {
+interface UserRanksProps {
+    readonly fadeDuration: number;
+    readonly quoteDuration: number;
     readonly track: RankedTrack;
-}): React.JSX.Element {
+    readonly visible: boolean;
+}
+
+export default function UserRanks(props: UserRanksProps): React.JSX.Element {
     const [scores, setScores] = React.useState<{
         user: string;
         notes: string;
         rank: number;
         place: number;
-    }[]>();
-    const [scoresVisible, setScoresVisible] = React.useState<number>();
+    }[]>([]);
+
+    const [scoresVisible, setScoresVisible] = React.useState<number>(0);
+    const scoreDelay = 5000;
 
     function getSuffix(number: number): string {
         const suffixes = ["st", "nd", "rd"];
@@ -20,48 +27,45 @@ export default function UserRanks(props: {
     }
 
     useEffect(() => {
-        setTimeout(() => {
-            setScoresVisible(0);
-            const userScores = shuffle(Object.entries(props.track.scores)).sort((a, b) => a[1].rank - b[1].rank).map(([key, value]) => ({
-                user: key,
-                notes: value.notes,
-                rank: value.rank,
-                place: 1
-            }));
-            for (let i = 0; i < userScores.length; i++) {
-                if (i > 0 && userScores[i].rank === userScores[i - 1].rank) {
-                    userScores[i].place = userScores[i - 1].place;
-                } else {
-                    userScores[i].place = i + 1;
-                }
+        const nQuotes = Object.values(props.track.scores).map((result) => result.notes).filter((note) => note !== "").length;
+        const userScores = shuffle(Object.entries(props.track.scores)).sort((a, b) => a[1].rank - b[1].rank).map(([key, value]) => ({
+            user: key,
+            notes: value.notes,
+            rank: value.rank,
+            place: 1
+        }));
+        for (let i = 0; i < userScores.length; i++) {
+            if (i > 0 && userScores[i].rank === userScores[i - 1].rank) {
+                userScores[i].place = userScores[i - 1].place;
+            } else {
+                userScores[i].place = i + 1;
             }
-            setScores(userScores);
-        }, 5000);
+        }
+        setScores(userScores);
+        setTimeout(() => {
+            setScoresVisible(1);
+        }, props.quoteDuration * (nQuotes + 1));
     }, [props.track]);
 
     useEffect(() => {
-        if (typeof scoresVisible === "number") {
-            if (scoresVisible < (scores?.length ?? 0)) {
-                setTimeout(() => {
-                    setScoresVisible((previous) => (previous ?? 0) + 1);
-                }, 3000);
-            }
+        if (scoresVisible < scores.length) {
+            setTimeout(() => setScoresVisible((previous) => previous + 1), scoreDelay);
         }
     }, [scoresVisible]);
 
     return (
-        <div className="mt-[6rem] flex gap-[12rem] justify-center">
-            {scores?.map((score) => (
-                <div className={`flex flex-col flex-1 text-center gap-3 w-[12rem] font-serif duration-1000
-                ${scores.length - score.place <= (scoresVisible ?? 0) ? "opacity-100" : "invisible opacity-0"}`} key={score.user}>
+        <div className="flex gap-[12rem] justify-center">
+            {scores.map((score) => (
+                <div className={`flex flex-col flex-1 text-center gap-3 w-[12rem] font-serif ease-in-out ${scores.length - score.place <= scoresVisible - 1 && props.visible ? "opacity-100" : "invisible opacity-0"}`} key={score.user}
+                    style={{ transitionDuration: `${props.fadeDuration}ms` }}>
                     <span className="text-5xl font-serif">
                         {score.rank}
                         <sup>{getSuffix(score.rank)}</sup>
                     </span>
-                    <span className="text-2xl mt-7">{score.user}</span>
-                    <span>{`“ ${score.notes.trim()} but the world goes round and round. ”`}</span>
+                    <span className="text-2xl mt-5">{score.user}</span>
+                    <span>{`“${score.notes.trim()}${(/[.!?]/gu).test(score.notes.trim().at(-1) ?? score.notes) ? "" : "."}”`}</span>
                 </div>
             ))}
-        </div>
+        </div >
     );
 }
