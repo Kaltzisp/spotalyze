7d4c3c0b97ac937f4b45e859a96456e39d6b0f00
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-14 18:53:22 +1100

    added quote formatting

--- src/app/soty/slideshow/Components/Quote.tsx
+++ src/app/soty/slideshow/Components/Quote.tsx
@@ -41,7 +41,7 @@ export default function Quote(props: QuoteProps): React.JSX.Element {
     return (
         <span className={`fixed pl-20 pr-20 text-5xl font-serif text-justify ease-in-out duration-${props.fadeDuration} ${quoteVisible ? "opacity-100" : "invisible opacity-0"}`}
             style={{ transitionDuration: `${props.fadeDuration}ms` }}>
-            {typeof quote === "undefined" ? null : `“${quote.trim()}”`}
+            {typeof quote === "undefined" ? null : `“${quote.trim()}${(/[.!?]/gu).test(quote.trim().at(-1) ?? "") ? "" : "."}”`}
         </span>
     );
 }
