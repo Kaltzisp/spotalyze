721768f3b19644546d3620a5aa059ac723971464
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-27 01:55:11 +1100

    implemented cookie backdoor

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,23 +2,17 @@ import { BadResponse } from "../../shared/utils";
 import type { NextRequest } from "next/server";
 
 export function GET(request: NextRequest): Response {
-    const clientToken = request.cookies.get("client_token");
-    const clientSecret = request.cookies.get("client_secret");
-    const redirectUri = request.cookies.get("redirect_uri");
-    if (typeof clientToken === "string" && typeof clientSecret === "string" && typeof redirectUri === "string") {
-        process.env.SPOTIFY_CLIENT_ID = clientToken;
-        process.env.SPOTIFY_CLIENT_SECRET = clientSecret;
-        process.env.SPOTIFY_REDIRECT_URI = redirectUri;
-        console.log(process.env.SPOTIFY_CLIENT_ID, process.env.SPOTIFY_CLIENT_SECRET);
+    const clientId = process.env.SPOTIFY_CLIENT_ID ?? request.cookies.get("client_id")?.value;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET ?? request.cookies.get("client_secret")?.value;
+    const redirectUri = process.env.SPOTIFY_REDIRECT_URI ?? request.cookies.get("redirect_uri")?.value;
+    if (typeof clientId === "string" && typeof clientSecret === "string" && typeof redirectUri === "string") {
+        const queryParams = new URLSearchParams({
+            client_id: clientId,
+            redirect_uri: redirectUri,
+            response_type: "code",
+            scope: "playlist-modify-public ugc-image-upload user-modify-playback-state"
+        });
+        return Response.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
     }
-    if (typeof process.env.SPOTIFY_CLIENT_ID === "undefined" || typeof process.env.SPOTIFY_CLIENT_SECRET === "undefined" || typeof process.env.SPOTIFY_REDIRECT_URI === "undefined") {
-        return new BadResponse("Missing Spotify credentials.", 400);
-    }
-    const queryParams = new URLSearchParams({
-        client_id: process.env.SPOTIFY_CLIENT_ID,
-        redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-        response_type: "code",
-        scope: "playlist-modify-public ugc-image-upload user-modify-playback-state"
-    });
-    return Response.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+    return new BadResponse("Missing Spotify credentials.", 400);
 }
--- src/app/api/auth/request-token/route.ts
+++ src/app/api/auth/request-token/route.ts
@@ -2,13 +2,16 @@ import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest): Promise<Response> {
     const authCode = request.nextUrl.searchParams.get("code");
+    const clientId = process.env.SPOTIFY_CLIENT_ID ?? request.cookies.get("client_id")?.value;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET ?? request.cookies.get("client_secret")?.value;
+    const redirectUri = process.env.SPOTIFY_REDIRECT_URI ?? request.cookies.get("redirect_uri")?.value;
     const apiResponse = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": `Basic ${btoa(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`)}`
+            "Authorization": `Basic ${btoa(`${clientId}:${clientSecret}`)}`
         },
-        body: `code=${authCode}&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&grant_type=authorization_code`
+        body: `code=${authCode}&redirect_uri=${redirectUri}&grant_type=authorization_code`
     });
     const data = await apiResponse.json() as { access_token: string };
     const response = NextResponse.redirect("http://localhost:3000/soty/home");
