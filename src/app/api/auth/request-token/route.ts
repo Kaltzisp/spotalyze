43eba714b4626a7f02742f77d011de330c021469
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-27 02:06:27 +1100

    updated redirect path to amplify app

--- src/app/api/auth/request-token/route.ts
+++ src/app/api/auth/request-token/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: NextRequest): Promise<Response> {
         body: `code=${authCode}&redirect_uri=${redirectUri}&grant_type=authorization_code`
     });
     const data = await apiResponse.json() as { access_token: string };
-    const response = NextResponse.redirect("http://localhost:3000/soty/home");
+    const response = NextResponse.redirect("https://main.d3hq5x9dp1vs3p.amplifyapp.com/soty/home");
     response.cookies.set("spotify_token", data.access_token, {
         path: "/",
         sameSite: "strict"
