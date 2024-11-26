cbfb14d4717b49e97dab81cc851dab1ee8692b70
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-27 01:08:37 +1100

    adding cookie backdoor for client tokens

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,14 @@
 import { BadResponse } from "../../shared/utils";
+import type { NextRequest } from "next/server";
 
-export function GET(): Response {
+export function GET(request: NextRequest): Response {
+    const clientToken = request.cookies.get("client_token");
+    const clientSecret = request.cookies.get("client_secret");
+    if (typeof clientToken === "string" && typeof clientSecret === "string") {
+        process.env.SPOTIFY_CLIENT_ID = clientToken;
+        process.env.SPOTIFY_CLIENT_SECRET = clientSecret;
+        console.log(process.env.SPOTIFY_CLIENT_ID, process.env.SPOTIFY_CLIENT_SECRET);
+    }
     if (typeof process.env.SPOTIFY_CLIENT_ID === "undefined" || typeof process.env.SPOTIFY_CLIENT_SECRET === "undefined" || typeof process.env.SPOTIFY_REDIRECT_URI === "undefined") {
         return new BadResponse("Missing Spotify credentials.", 400);
     }
