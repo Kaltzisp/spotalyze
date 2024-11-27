fb7e82c56ef26f637fd4c72f78952186d0372676
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-28 00:21:06 +1100

    removed cookie backdoor

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,18 +1,14 @@
-import { BadResponse } from "../../shared/utils";
-import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export function GET(request: NextRequest): Response {
-    const clientId = process.env.SPOTIFY_CLIENT_ID ?? request.cookies.get("client_id")?.value;
-    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET ?? request.cookies.get("client_secret")?.value;
-    const redirectUri = process.env.SPOTIFY_REDIRECT_URI ?? request.cookies.get("redirect_uri")?.value;
-    if (typeof clientId === "string" && typeof clientSecret === "string" && typeof redirectUri === "string") {
-        const queryParams = new URLSearchParams({
-            client_id: clientId,
-            redirect_uri: redirectUri,
-            response_type: "code",
-            scope: "playlist-modify-public ugc-image-upload user-modify-playback-state"
-        });
-        return Response.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+export function GET(): Response {
+    if (typeof process.env.SPOTIFY_CLIENT_ID === "undefined" || typeof process.env.SPOTIFY_CLIENT_SECRET === "undefined" || typeof process.env.SPOTIFY_REDIRECT_URI === "undefined") {
+        return NextResponse.json({ error: "Missing environment variables" }, { status: 500 });
     }
-    return new BadResponse("Missing Spotify credentials.", 400);
+    const queryParams = new URLSearchParams({
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+        response_type: "code",
+        scope: "playlist-modify-public ugc-image-upload user-modify-playback-state"
+    });
+    return Response.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 }
--- src/app/api/auth/request-token/route.ts
+++ src/app/api/auth/request-token/route.ts
@@ -2,19 +2,16 @@ import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest): Promise<Response> {
     const authCode = request.nextUrl.searchParams.get("code");
-    const clientId = process.env.SPOTIFY_CLIENT_ID ?? request.cookies.get("client_id")?.value;
-    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET ?? request.cookies.get("client_secret")?.value;
-    const redirectUri = process.env.SPOTIFY_REDIRECT_URI ?? request.cookies.get("redirect_uri")?.value;
     const apiResponse = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": `Basic ${btoa(`${clientId}:${clientSecret}`)}`
+            "Authorization": `Basic ${btoa(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`)}`
         },
-        body: `code=${authCode}&redirect_uri=${redirectUri}&grant_type=authorization_code`
+        body: `code=${authCode}&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&grant_type=authorization_code`
     });
     const data = await apiResponse.json() as { access_token: string };
-    const response = NextResponse.redirect("https://main.d3hq5x9dp1vs3p.amplifyapp.com/soty/home");
+    const response = NextResponse.redirect(`${request.nextUrl.origin}/soty/home`);
     response.cookies.set("spotify_token", data.access_token, {
         path: "/",
         sameSite: "strict"
