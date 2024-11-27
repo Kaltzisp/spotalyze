97895d28da8e5bba19e4c502076b1335de09453c
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-28 10:15:51 +1100

    improved create-playlist logic

--- src/app/api/playlists/create/route.ts
+++ src/app/api/playlists/create/route.ts
@@ -1,16 +1,27 @@
-import { BadResponse } from "../../shared/utils";
+import { BadResponse, shuffle } from "../../shared/utils";
 import type { NextRequest } from "next/server";
 import { Playlist } from "../../shared/Playlist";
 
-export async function GET(request: NextRequest): Promise<Response> {
-    const trackString = request.nextUrl.searchParams.get("track_ids");
-    const trackURIs = trackString?.split(",").map((trackId) => `spotify:track:${trackId}`);
+interface CreatePlaylistBody {
+    sourcePlaylistUrl?: string;
+    trackURIs?: string[];
+    shuffle: boolean;
+}
+
+export async function POST(request: NextRequest): Promise<Response> {
+    const body = await request.json() as CreatePlaylistBody;
     const spotifyToken = request.cookies.get("spotify_token")?.value;
-    if (typeof trackURIs === "undefined") {
-        return new BadResponse("Malformed track IDs object.", 400);
-    } else if (typeof spotifyToken === "undefined") {
+    if (typeof spotifyToken === "undefined") {
         return new BadResponse("Authentication required.", 401);
     }
-    const playlist = await Playlist.fromTracks("Spotalyze Playlist", trackURIs, null, spotifyToken);
-    return new Response(`Playlist created at ${playlist.url}`, { status: 201 });
+    if (typeof body.sourcePlaylistUrl === "string") {
+        const playlist = await Playlist.fromUrl(body.sourcePlaylistUrl, spotifyToken);
+        body.trackURIs = playlist.trackURIs;
+    }
+    if (typeof body.trackURIs !== "undefined") {
+        const uris = body.shuffle ? shuffle(body.trackURIs) : body.trackURIs;
+        const playlist = await Playlist.fromTracks("Spotalyze Playlist", uris, null, spotifyToken);
+        return new Response(`Playlist created at ${playlist.url}`, { status: 201 });
+    }
+    return new BadResponse("Missing track URIs.", 500);
 }
