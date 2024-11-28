2f26a0226a01716f6fab1eb0ed3806eb908d74e8
Peter Kaltzis <peterkaltzis@outlook.com>
2024-11-28 11:41:04 +1100

    use soty/home components

--- src/app/soty/home/page.tsx
+++ src/app/soty/home/page.tsx
@@ -1,109 +1,30 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import type { Playlist } from "@/app/lib/Playlist";
-import { useRouter } from "next/navigation";
-
-export interface TextFile {
-    name: string;
-    content: string;
-}
+import Dropzone from "./Components/Dropzone";
+import UtilButtons from "./Components/UtilButtons";
 
 
 export default function Spotalyze(): React.JSX.Element {
-    const router = useRouter();
-    const [authButtonClass, setAuthButtonClass] = useState("color-norm");
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [textInput, setTextInput] = useState("");
-    const [showDropbox, setShowDropbox] = useState(false);
-    const [files, setFiles] = useState<TextFile[]>([]);
 
-    useEffect(() => { setAuthButtonClass(document.cookie.startsWith("spotify_token=") ? "color-auth" : "color-unauth") }, []);
+    useEffect(() => { setIsAuthenticated(document.cookie.split("; ").some((cookie) => cookie.startsWith("spotify_token="))) }, []);
     useEffect(() => { setTextInput(localStorage.getItem("textInput") ?? "") }, []);
     useEffect(() => { localStorage.setItem("textInput", textInput) }, [textInput]);
-
-    function handleFileDrop(event: React.DragEvent<HTMLDivElement>): void {
-        event.preventDefault();
-        for (const file of event.dataTransfer.files) {
-            const reader = new FileReader();
-            reader.onload = (): void => {
-                setFiles((previousFiles) => [...previousFiles, {
-                    name: file.name.split(".")[0],
-                    content: reader.result as string
-                }]);
-            };
-            reader.readAsText(file);
-        }
-    }
-
-    async function handleFileSumbission(): Promise<void> {
-        setShowDropbox(false);
-        const response = await fetch(`/api/playlists/submit-results?url=${textInput}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(files)
-        });
-        const playlist = await response.json() as Playlist;
-        localStorage.setItem("Playlist", JSON.stringify(playlist));
-        router.push("/soty/slideshow");
-    }
 
     return (
         <div className="align-middle justify-items-center p-8">
             <main className="flex flex-col gap-8">
                 <span className="flex justify-center gap-5">
-                    <button className={`nice-button ${authButtonClass}`} type="button" onClick={() => {
+                    <button className={`nice-button ${isAuthenticated ? "color-auth" : "color-unauth"}`} type="button" onClick={() => {
                         window.location.href = "/api/auth/login";
                     }}>{"Authenticate"}
                     </button>
-                    <input className="nice-button color-norm" placeholder="Spotify URL" type="text" value={textInput} onChange={(event) => setTextInput(event.target.value)} />
+                    <input className="nice-button color-norm" placeholder="Playlist URL / Track IDs" type="text" value={textInput} onChange={(event) => setTextInput(event.target.value)} />
                 </span>
                 <p className="text-center">{"Spotalyze. © SOTY 2024"}</p>
-                <span className="flex justify-center">
-                    <button className="nice-button color-invert mr-5" type="button" onClick={() => {
-                        fetch(`/api/playlists/get-csv?url=${textInput}`).catch((error: unknown) => console.error(error));
-                    }}>{"To CSV"}
-                    </button>
-                    <button className="nice-button color-invert mr-5" type="button" onClick={() => {
-                        fetch("/api/playlists/create", {
-                            method: "POST",
-                            body: JSON.stringify({
-                                trackURIs: textInput.split(",").map((uri) => uri.trim()),
-                                shuffle: false
-                            })
-                        }).catch((error: unknown) => console.error(error));
-                    }}>{"To Playlist"}
-                    </button>
-                    <button className="nice-button color-invert mr-5" type="button" onClick={() => {
-                        fetch("/api/playlists/create", {
-                            method: "POST",
-                            body: JSON.stringify({
-                                sourcePlaylistUrl: textInput,
-                                shuffle: true
-                            })
-                        }).catch((error: unknown) => console.error(error));
-                    }}>{"Shuffle"}
-                    </button>
-                    <button className="nice-button color-invert" type="button" onClick={() => {
-                        fetch(`/api/playlists/get-info?url=${textInput}`).catch((error: unknown) => console.error(error));
-                    }}>{"Get Info"}
-                    </button>
-                </span>
-                {showDropbox ? <div className="rounded-full px-4 flex justify-center items-center drag-drop-area border h-20"
-                    onDragOver={(event) => event.preventDefault()} onDrop={(event) => handleFileDrop(event)}>
-                    {files.length > 0 ? <ul className="columns-4 font-mono flex">
-                        {files.map((file) => <li className="text-center m-3" key={file.name}>{file.name}</li>)}
-                    </ul> : null}
-                </div> : null}
-                <button className="nice-button color-invert" type="button" onClick={() => {
-                    if (showDropbox) {
-                        handleFileSumbission().catch((error: unknown) => console.error(error));
-                        setShowDropbox(false);
-                    } else {
-                        setShowDropbox(true);
-                    }
-                }}>{showDropbox ? "Submit Files" : "Upload Files"}
-                </button>
+                <UtilButtons textInput={textInput} />
+                <Dropzone textInput={textInput} />
             </main>
         </div>
     );
